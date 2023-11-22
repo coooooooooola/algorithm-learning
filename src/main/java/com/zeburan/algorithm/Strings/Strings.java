4775eec8f6ed03c24a35574cf453fa215835debb
zeburan <swtywang@tju.edu.cn>
2023-11-22 14:28:06 +0800

    update

--- src/main/java/com/zeburan/algorithm/Strings/Strings.java
+++ src/main/java/com/zeburan/algorithm/Strings/Strings.java
@@ -6,10 +6,10 @@ package com.zeburan.algorithm.Strings;
 public class Strings {
     /**
      * Leetcode 28:https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+     *
      * @param haystack
      * @param needle
-     * @return
-     * 自己实现indexOf
+     * @return 自己实现indexOf
      */
     public int strStr(String haystack, String needle) {
         //return haystack.indexOf(needle);
@@ -34,6 +34,7 @@ public class Strings {
     /**
      * 58. 最后一个单词的长度 - 普通解法
      * https://leetcode.cn/problems/length-of-last-word/description/?envType=study-plan-v2&envId=top-interview-150
+     *
      * @param s
      * @return
      */
@@ -48,6 +49,7 @@ public class Strings {
     /**
      * ⭐ 58. 最后一个单词的长度 - 优化解法
      * https://leetcode.cn/problems/length-of-last-word/description/?envType=study-plan-v2&envId=top-interview-150
+     *
      * @param s
      * @return
      */
@@ -55,11 +57,36 @@ public class Strings {
         String s1 = s.trim();
         int len = s1.length();
 
-        for (int i=len-1;i>=0;i--){
-            if (s1.charAt(i)==' '){
-                return len-1-i;
+        for (int i = len - 1; i >= 0; i--) {
+            if (s1.charAt(i) == ' ') {
+                return len - 1 - i;
             }
         }
         return len;
+    }
+
+
+    public String longestCommonPrefix(String[] strs) {
+        String result = strs[0];
+        for (int i=1;i<strs.length;i++){
+            result = prefix(result, strs[i]);
+            if (result.isEmpty()){
+                break;
+            }
+        }
+        return result;
+    }
+
+    public String prefix(String s1, String s2) {
+        int index = 0;
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int minLength = Math.min(len1, len2);
+        for (int i = 0; i < minLength; i++) {
+            if (s1.charAt(i)!=s2.charAt(i)){
+                return s1.substring(0,i);
+            }
+        }
+        return s1.substring(0,minLength);
     }
 }
